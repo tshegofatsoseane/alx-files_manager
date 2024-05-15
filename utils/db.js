f5e55a43acf58482178e032e9587038b7684abc1
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2024-05-15 23:09:04 +0000

    more utility functions

---
+++ utils/db.js
@@ -0,0 +1,66 @@
+import mongodb from 'mongodb';
+// eslint-disable-next-line no-unused-vars
+import Collection from 'mongodb/lib/collection';
+import envLoader from './env_loader';
+
+/**
+ * Represents MongoDB client.
+ */
+class DBClient {
+  /**
+   * Start a new DBClient instance.
+   */
+  constructor() {
+    envLoader();
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const dbURL = `mongodb://${host}:${port}/${database}`;
+
+    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  /**
+   * Verify the client's connection to the MongoDB server.
+   * @returns {boolean}
+   */
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  /**
+   * Gets the number of users in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbUsers() {
+    return this.client.db().collection('users').countDocuments();
+  }
+
+  /**
+   * Gets the number of files in the database.
+   * @returns {Promise<Number>}
+   */
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+
+  /**
+   * Gets the reference to the `users` collection.
+   * @returns {Promise<Collection>}
+   */
+  async usersCollection() {
+    return this.client.db().collection('users');
+  }
+
+  /**
+   * Gets the reference to the `files` collection.
+   * @returns {Promise<Collection>}
+   */
+  async filesCollection() {
+    return this.client.db().collection('files');
+  }
+}
+
+export const dbClient = new DBClient();
+export default dbClient;
---
+++ utils/mailer.js
@@ -0,0 +1,145 @@
+/* eslint-disable no-unused-vars */
+import fs from 'fs';
+import readline from 'readline';
+import { promisify } from 'util';
+import mimeMessage from 'mime-message';
+import { gmail_v1 as gmailV1, google } from 'googleapis';
+
+const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
+const TOKEN_PATH = 'token.json';
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+/**
+ * Retrieve and store token after prompting for user autho
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client.
+ * @param {getEventsCallback} callback The callback of authorized client.
+ */
+async function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        console.error('Error retrieving access token', err);
+        return;
+      }
+      oAuth2Client.setCredentials(token);
+      writeFileAsync(TOKEN_PATH, JSON.stringify(token))
+        .then(() => {
+          console.log('Token stored to', TOKEN_PATH);
+          callback(oAuth2Client);
+        })
+        .catch((writeErr) => console.error(writeErr));
+    });
+  });
+}
+
+/**
+ * Make an OAuth2 client with the credentials.
+ * @param {Object} credentials - Authorization client credentials.
+ * @param {function} callback - Callback to call the auth client.
+ */
+async function authorize(credentials, callback) {
+  const clientSecret = credentials.web.client_secret;
+  const clientId = credentials.web.client_id;
+  const redirectURIs = credentials.web.redirect_uris;
+  const oAuth2Client = new google.auth.OAuth2(
+    clientId,
+    clientSecret,
+    redirectURIs[0],
+  );
+  console.log('Client authorization beginning');
+  // Check if we have previously stored a token.
+  await readFileAsync(TOKEN_PATH)
+    .then((token) => {
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    }).catch(async () => getNewToken(oAuth2Client, callback));
+  console.log('Client authorization done');
+}
+
+/**
+ * Send mail through the user account.
+ * @param {google.auth.OAuth2} auth - The authorized OAuth2 client.
+ * @param {gmailV1.Schema$Message} mail - The message.
+ */
+function sendMailService(auth, mail) {
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  gmail.users.messages.send({
+    userId: 'me',
+    requestBody: mail,
+  }, (err, _res) => {
+    if (err) {
+      console.log(`The API returned an error: ${err.message || err.toString()}`);
+      return;
+    }
+    console.log('Message sent successfully');
+  });
+}
+
+/**
+ * Routines for mail delivery with GMail.
+ */
+export default class Mailer {
+  static checkAuth() {
+    readFileAsync('credentials.json')
+      .then(async (content) => {
+        await authorize(JSON.parse(content), (auth) => {
+          if (auth) {
+            console.log('Auth check was successful');
+          }
+        });
+      })
+      .catch((err) => {
+        console.log('Error loading client secret file:', err);
+      });
+  }
+
+  static buildMessage(dest, subject, message) {
+    const senderEmail = process.env.GMAIL_SENDER;
+    const msgData = {
+      type: 'text/html',
+      encoding: 'UTF-8',
+      from: senderEmail,
+      to: [dest],
+      cc: [],
+      bcc: [],
+      replyTo: [],
+      date: new Date(),
+      subject,
+      body: message,
+    };
+
+    if (!senderEmail) {
+      throw new Error(`Invalid sender: ${senderEmail}`);
+    }
+    if (mimeMessage.validMimeMessage(msgData)) {
+      const mimeMsg = mimeMessage.createMimeMessage(msgData);
+      return { raw: mimeMsg.toBase64SafeString() };
+    }
+    throw new Error('Invalid MIME message');
+  }
+
+  static sendMail(mail) {
+    readFileAsync('credentials.json')
+      .then(async (content) => {
+        await authorize(
+          JSON.parse(content),
+          (auth) => sendMailService(auth, mail),
+        );
+      })
+      .catch((err) => {
+        console.log('Error loading client secret file:', err);
+      });
+  }
+}
